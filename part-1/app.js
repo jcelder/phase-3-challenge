9eceec29b0cf786b7800d504154662e6ead8203e
Joshua Elder <aresx130@gmail.com>
2017-10-30 14:06:25 -0700

    Added routes and linked routes to app.js.

--- part-1/app.js
+++ part-1/app.js
@@ -1,7 +1,11 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 
+const { router } = require('./routes/routes.js')
+
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/api', router)
 
 app.listen(3000, () => {
   console.log('Application is running on localhost:3000')
---
+++ part-1/routes/routes.js
@@ -0,0 +1,11 @@
+const router = require('express').router
+
+router.get('/days/:day', (req, res) => {
+
+})
+
+router.post('/array/concat', (req, res) => {
+
+})
+
+module.exports = { router }
