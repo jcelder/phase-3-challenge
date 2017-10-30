a2c4b31bb42e58abe1abd85ff09a15de283811c3
Joshua Elder <aresx130@gmail.com>
2017-10-30 14:12:57 -0700

    Removed references to removed routes/routes.js file and added basic route structure.

--- part-1/app.js
+++ part-1/app.js
@@ -1,11 +1,15 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 
-const { router } = require('./routes/routes.js')
-
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/api', router)
+app.get('/api/days/:day', (req, res) => {
+
+})
+
+app.post('/api/array/concat', (req, res) => {
+  
+})
 
 app.listen(3000, () => {
   console.log('Application is running on localhost:3000')
