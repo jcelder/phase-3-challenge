68ac81c657319d8d70b43c112f800ff1c7f3489b
Joshua Elder <aresx130@gmail.com>
2017-10-30 23:24:18 -0700

    Finished /api/days/:day route.

--- part-1/app.js
+++ part-1/app.js
@@ -1,14 +1,29 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 
+const daysOfWeek = {
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6,
+  sunday: 7,
+}
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/api/days/:day', (req, res) => {
+  const day = req.params.day
 
+  res.set('Content-Type', 'text/plain')
+  daysOfWeek[day] === undefined
+    ? res.status(400).send(`'${day}' is not a valid day!`)
+    : res.send(`${daysOfWeek[day]}`)
 })
 
 app.post('/api/array/concat', (req, res) => {
-  
+
 })
 
 app.listen(3000, () => {
