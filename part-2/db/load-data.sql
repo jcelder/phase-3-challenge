652688bc16bea15e90f3008188578777607130de
Joshua Elder <aresx130@gmail.com>
2017-10-31 22:59:23 -0700

    Added loading from csv file.

--- part-2/db/load-data.sql
+++ part-2/db/load-data.sql
@@ -1,12 +1,5 @@
--- Sections
-INSERT INTO sections (section_name) VALUES
-  ('Dairy'),
-  ('Produce'),
-  ('Meat'),
-  ('Bakery'),
-  ('Frozen');
+COPY products (name, price, section) FROM '/Users/josh/dev/phase2/phase-3-challenge/part-2/db/grocery.csv' WITH (FORMAT csv, HEADER true);
 
--- Shoppers
 INSERT INTO shoppers (shopper_name) VALUES
   ('Josh'),
   ('Jon'),
@@ -14,47 +7,7 @@ INSERT INTO shoppers (shopper_name) VALUES
   ('Corey'),
   ('Joe');
 
--- Products (Dairy)
-INSERT INTO products (product_name, product_price, section_id) VALUES
-  ('Milk', '3.99', '1'),
-  ('Butter', '2.99', '1'),
-  ('Cheddar Cheese', '4.99', '1'),
-  ('Cream Cheese', '3.49', '1'),
-  ('Yogurt', '1.49', '1');
 
--- Products (Produce)
-INSERT INTO products (product_name, product_price, section_id) VALUES
-  ('Carrot', '0.99', '2'),
-  ('Onion', '0.79', '2'),
-  ('Potato', '0.89', '2'),
-  ('Broccoli', '0.69', '2'),
-  ('Garlic', '0.89', '2');
-
--- Products (Meat)
-INSERT INTO products (product_name, product_price, section_id) VALUES
-  ('Chicken', '6.99', '3'),
-  ('Ground Beef', '4.99', '3'),
-  ('Bacon', '3.49', '3'),
-  ('Ground Turkey', '5.99', '3'),
-  ('Beef Sirloin', '9.99', '3');
-
--- Products (Bakery)
-INSERT INTO products (product_name, product_price, section_id) VALUES
-  ('French Rolls(12)', '4.99', '4'),
-  ('White Bread', '2.99', '4'),
-  ('Whole Wheat Bread', '3.99', '4'),
-  ('Sourdough Loaf', '5.99', '4'),
-  ('Hot Dog Buns(24)', '4.49', '4');
-
--- Products (Frozen)
-INSERT INTO products (product_name, product_price, section_id) VALUES
-  ('Cheese Pizza', '7.99', '5'),
-  ('Pepperoni Pizza', '8.49', '5'),
-  ('Vegetable Stir-Fry', '8.99', '5'),
-  ('Orange Chicken', '11.99', '5'),
-  ('Chicken Tenders', '8.99', '5');
-
--- orders
 INSERT INTO orders (shopper_id) VALUES
   ('1'),
   ('3'),
@@ -63,7 +16,7 @@ INSERT INTO orders (shopper_id) VALUES
   ('2'),
   ('5');
 
--- order_items
+
 INSERT INTO order_items (order_id, product_id) VALUES
   ('1', '1'),
   ('1', '24'),
