35031574c5ed486956d02ee5e9c5d009ae07f6e1
Joshua Elder <aresx130@gmail.com>
2017-10-31 20:35:08 -0700

    Changed cart-button class to cart-btn and updated all references.

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,6 +1,10 @@
 
-const cartBtn = document.getElementById('cart-button')
+// Button Selectors
+const cartBtn = document.getElementById('cart-btn')
 const closeBtn = document.querySelector('.close-btn')
+const clearBtn = document.querySelector('.clear-btn')
+const addBtns = document.querySelectorAll('.item-btn')
+//
 const modal = document.getElementById('cartModal')
 
 cartBtn.addEventListener('click', () => modal.style.display = 'block')
