9561460a17558905086551473e416c9f54bc081c
Joshua Elder <aresx130@gmail.com>
2017-11-01 01:50:03 -0700

    Finally got the asynchronous promise resolution handled and the listProductsBySection test is now passing.

--- part-2/test/database_test.js
+++ part-2/test/database_test.js
@@ -1,7 +1,21 @@
 const { expect } = require('chai')
 
 const { db, pgp } = require('../db/db_connection.js')
+const { initDB } = require('./db/database_test_utils.js')
+const { listProductsBySection } = require('../db/database.js')
 
 describe('listProductsBySection()', () => {
-
+  const section = 'Dairy'
+  let listPromise
+  beforeEach('reset the database', (done) => {
+    listPromise = initDB()
+      .then(() => listProductsBySection(section))
+    done()
+  })
+  it('should return an array of length 3 when given the dairy section', () => {
+    return listPromise.then((results) => {
+      console.log(results)
+      expect(results.length).to.equal(3)
+    })
+  })
 })
