79a763a406d78f7416a55419e76adaa04850796c
Joshua Elder <aresx130@gmail.com>
2017-11-01 01:48:47 -0700

    Added and finalized db initialization and seeding functions.

--- part-2/test/db/database_test_utils.js
+++ part-2/test/db/database_test_utils.js
@@ -9,9 +9,70 @@ const resetDB = () => {
 }
 
 /**
- * Seeds shoppers tables in the database with the test data
- * @returns {Promise} - Promise whos resolution is irrelevant
+ * Seeds shoppers tables in the test database
+ * @returns {Promise} - Promise whose resolution is irrelevant
  */
 const seedShoppers = () => {
-  return db.any('INSERT INTO shoppers')
+  return db.any(`INSERT INTO shoppers (shopper_name) VALUES
+    ('Josh'),
+    ('Jon'),
+    ('Vincenzo')`)
 }
+
+/**
+ * Seeds products table in the test database
+ * @returns {Promise} - Promise whose resolution is irrelevant
+ */
+const seedProducts = () => {
+  return db.any(`INSERT INTO products (name, price, section) VALUES
+  ('Milk', '5.99', 'Dairy'),
+  ('Butter', '3.99', 'Dairy'),
+  ('Cheddar Cheese', '6.99', 'Dairy'),
+  ('Onions', '1.29', 'Produce'),
+  ('Garlic', '1.39', 'Produce'),
+  ('Chicken Breast', '4.29', 'Meat')`)
+}
+
+/**
+ * Seeds orders table in the test database
+ * @returns {Promise} - Promise whose resolution is irrelevant
+ */
+const seedOrders = () => {
+  return db.any(`INSERT INTO orders (shopper_id) VALUES
+  ('1'),
+  ('2'),
+  ('1')`)
+}
+
+/**
+ * Seeds order_items join table in the test database
+ * @returns {Promise} - Promise whose resolution is irrelevant
+ */
+const seedOrderItems = () => {
+  return db.any(`INSERT INTO order_items (order_id, product_id) VALUES
+  ('1', '2'),
+  ('2', '4'),
+  ('3', '1'),
+  ('3', '3')`)
+}
+
+/**
+ * Seeds all tables in database
+ * @returns {Promise} - Promise whose resolution is irrelevant
+ */
+const seedDB = () => {
+  return seedShoppers()
+    .then(seedProducts)
+    .then(seedOrders)
+    .then(seedOrderItems)
+}
+
+/**
+ * Resets then seeds the database
+ * @returns {Promise} - Promise whose resolutions is irrelevant
+ */
+const initDB = () => {
+  return resetDB().then(seedDB)
+}
+
+module.exports = { initDB }
